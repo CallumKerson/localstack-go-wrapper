0ec02847574d2e698e1687eb402907378866cd88
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2019-01-14 15:00:59 +0000

    Commenting out custom port

--- integration/localstack_s3_test.go
+++ integration/localstack_s3_test.go
@@ -16,7 +16,7 @@ func TestLocalstackS3Started(t *testing.T) {
 	sess, err := session.NewSession(&aws.Config{
 		Credentials: testCredentials,
 		Region:      testRegion,
-		Endpoint:    aws.String("http://localhost:" + strconv.Itoa(s3Port)), // uses custom s3 port
+		Endpoint:    aws.String("http://localhost:4572" //+ strconv.Itoa(s3Port)), // uses custom s3 port
 		DisableSSL:  disableSSL,
 	})
 	if err != nil {
