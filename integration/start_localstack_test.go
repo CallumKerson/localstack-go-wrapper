927f032dde450168273218f27d182450d0ca96b8
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2019-01-15 12:33:06 +0000

    Remove testing.Short check for start localstack

--- integration/start_localstack_test.go
+++ integration/start_localstack_test.go
@@ -17,31 +17,29 @@ func TestMain(m *testing.M) {
 }
 
 func testMainWrapper(m *testing.M) int {
-	if !testing.Short() {
-		s3Config := &services.ServiceConfig{
-			Service: services.S3,
-			Port:    s3Port,
-		}
-		log.Printf("Creating localstack S3 config with port %v", s3Port)
-		sqsConfig := &services.ServiceConfig{
-			Service: services.SQS,
-		}
-		log.Print("Creating localstack SQS config with port default port")
-		snsConfig := &services.ServiceConfig{
-			Service: services.SNS,
-		}
-		log.Print("Creating localstack SNS config with port default port")
-		container, err := localstack.New(s3Config, sqsConfig, snsConfig)
-		if err != nil {
-			log.Printf("Cannot create localstack, %v", err)
-			return 1
-		}
-		err = container.Start()
-		if err != nil {
-			log.Printf("Cannot start localstack, %v", err)
-			return 1
-		}
-		defer container.Stop()
+	s3Config := &services.ServiceConfig{
+		Service: services.S3,
+		Port:    s3Port,
 	}
+	log.Printf("Creating localstack S3 config with port %v", s3Port)
+	sqsConfig := &services.ServiceConfig{
+		Service: services.SQS,
+	}
+	log.Print("Creating localstack SQS config with port default port")
+	snsConfig := &services.ServiceConfig{
+		Service: services.SNS,
+	}
+	log.Print("Creating localstack SNS config with port default port")
+	container, err := localstack.New(s3Config, sqsConfig, snsConfig)
+	if err != nil {
+		log.Printf("Cannot create localstack, %v", err)
+		return 1
+	}
+	err = container.Start()
+	if err != nil {
+		log.Printf("Cannot start localstack, %v", err)
+		return 1
+	}
+	defer container.Stop()
 	return m.Run()
 }
